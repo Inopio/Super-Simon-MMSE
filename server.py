b6ad18d46afb7a78997c48ce9260acf16e03a328
Sarah Portejoie <vasawin09@gmail.com>
2021-01-07 16:01:08 +0100

    Adding game file

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import socket
 from _thread import *
 
 nbplayer = 0
+running = True
 
 def threadClient(ip, port, socket):
 
@@ -17,34 +18,34 @@ def threadClient(ip, port, socket):
             print("Connection from : %s %s" % (ip, port, ))
 
             if(nbplayer >= 2) :
+                running = True
                 data = "good"
                 socket.send(str.encode(data))
 
         except:
             break
     print("Lost connexion")
+    running = False
     socket.close()
 
+if __name__ == "__main__":
+    tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    tcpsock.bind(("127.0.0.1", 2001))
 
-    #print("Client déconnecté...")
-
-tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-tcpsock.bind(("127.0.0.1", 2001))
-
-
-
-while True:
-    tcpsock.listen(10)
-    print( "Listening")
-    (socket, (ip, port)) = tcpsock.accept()
-    nbplayer = nbplayer + 1
-    print( "next")
+    while True:
+        if(not running):
+            break
+        tcpsock.listen(10)
+        print( "Listening")
+        (socket, (ip, port)) = tcpsock.accept()
+        nbplayer = nbplayer + 1
+        print( "next")
 
 
-    if nbplayer < 2 :
-        print("Waiting for at least two players...")
-    else:
-        print("Enough player has joined, we can start :)")
+        if nbplayer < 2 :
+            print("Waiting for at least two players...")
+        else:
+            print("Enough player has joined, we can start :)")
 
-    start_new_thread(threadClient,(ip, port, socket))
+        start_new_thread(threadClient,(ip, port, socket))
