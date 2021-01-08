77cf951abf4cfb099d29d210ddac0c36a506676e
Sarah Portejoie <vasawin09@gmail.com>
2021-01-08 17:25:49 +0100

    Saved number of player client.py

--- client.py
+++ client.py
@@ -3,17 +3,18 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('127.0.0.1', 2001))
 
-
+#receivedMyNb = False
+nbPlayer = -1
 
 while(True):
+    s.send(str.encode("game"))
     try:
-        s.send(str.encode("game"))
         data = s.recv(2048).decode()
         if not data :
             print("Null received")
             break
         else :
-            print(data)
+            #print(data)
             if(data == "good"):
                 print("WOUHOU!")
 
@@ -21,6 +22,7 @@ while(True):
                 nbPlayer = data
                 print("var from server")
                 print("nbplayer = ", nbPlayer)
+                receivedMyNb = True
                 #break
 
             else:
@@ -29,34 +31,3 @@ while(True):
     except:
         print("Connection closed")
         break
-
-
-
-''' TODO:
-- Check l'envoi utilisateur -> checkMove()
-envoyer au joueur via Serial COM le joueur actuel
-pour afficher sur LCD later '=-> sendToArduino()
-
-- Ameliorer la prise en charge du nombre de participant
-qui n'est pas encore fonctionnelle
-''' 
-'''
-def round():
-    global pattern
-    global nbRounds
-
-    for i in range (requiredNbPlayers):
-        player = pattern.append(random.randint(a=1, b=requiredNbPlayers))
-        print(player)
-    nbRounds = len(pattern) / requiredNbPlayers
-'''
-
-''' 
-if(finishedRound):
-    print("You passed the round ",nbRounds)
-    round()
-
-elif(isGameOver):
-    pattern = []
-    nbRounds = 0
-    round()'''
