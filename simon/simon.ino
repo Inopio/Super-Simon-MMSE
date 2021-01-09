143a11a1e977d03f5166056e38b9879e55191469
ebesse <emmahbesse@gmail.com>
2021-01-09 12:17:08 +0100

    Game mostly working (missing on action/unexplained lost games)

--- simon/simon.ino
+++ simon/simon.ino
@@ -14,7 +14,11 @@ int player[] = {0,1,2,3};
 int pos[] = {22,67,112,157};
 long dir[length];
 long order[length];
+<<<<<<< HEAD
+int streak,ran,currpos,wait,action;
+=======
 int streak,ran,currpos,wait;
+>>>>>>> 682a301cec1ae3c69ab81c8ecdb5885f4772a042
 int previous = -1;
 bool lost = false;
 bool played = true;
@@ -51,6 +55,11 @@ void setup() {
   lcd.setCursor(0, 1);// pour afficher sur la deuxième ligne
   lcd.print("Game");
   delay(2000); //pour avoir le temps de voir le message s'afficher
+
+  //ultrason
+  pinMode(trigPin,OUTPUT);
+  digitalWrite(trigPin,LOW);
+  pinMode(echoPin,INPUT);
   
   //interrupteur
    pinMode(interrupt, INPUT);
@@ -69,9 +78,15 @@ void setup() {
   streak = 0;
   randomSeed(analogRead(0));
   for (int i = 0; i < length; i+=1){
+<<<<<<< HEAD
+    ran = random(3);
+    while(ran == previous){
+      ran = random(3);
+=======
     ran = random(1,3);
     while(ran == previous){
       ran = random(1,3);
+>>>>>>> 682a301cec1ae3c69ab81c8ecdb5885f4772a042
     }
     order[i] = ran;
     dir[i] = pos[ran];
@@ -109,12 +124,15 @@ void loop(){
       }
     }
     currpos = dir[i];
-    Serial.print(currpos);
+    Serial.print(order[i]);
     Serial.print('\n');
     delay(500);
   }
   //streak++;
   played = false;
+<<<<<<< HEAD
+  pressed = false;
+  action = 0;
   }
   else if(lost){
     lcd.clear();
@@ -123,6 +141,16 @@ void loop(){
     lcd.print("Score ");
     lcd.print(streak);
   }
+=======
+  }
+  else if(lost){
+    lcd.clear();
+    lcd.print("Game lost");
+    lcd.setCursor(0,1);
+    lcd.print("Score ");
+    lcd.print(streak);
+  }
+>>>>>>> 682a301cec1ae3c69ab81c8ecdb5885f4772a042
   
   //fin servo
   
@@ -137,6 +165,29 @@ void loop(){
   float distance = measure*0.0340 / 2.0  ;
   
   //on affiche la distance
+<<<<<<< HEAD
+  //Serial.print("distance = ");
+  //Serial.println(distance);
+  if(distance < 5 && !played){ //action
+    if (order[action] == 0){
+      if(action == streak){
+        streak ++;
+        Serial.println("Performed :");
+        Serial.println(0);
+        played = true;
+      }
+      else{
+        action ++;
+        Serial.println("Performed :");
+        Serial.println(0);
+      }
+    }
+    else{
+      lost = true;
+      Serial.println("lost ultrason");
+    }
+  }
+=======
   Serial.print("distance = ");
   Serial.println(distance);
 //  if(distance < 5 && !played){ //action
@@ -148,6 +199,7 @@ void loop(){
 //    }
 //    played = true;
 //  }
+>>>>>>> 682a301cec1ae3c69ab81c8ecdb5885f4772a042
   
   //fin ultrason 
   
@@ -157,6 +209,25 @@ void loop(){
   if( state == 0 && !closed && !played){
     //Serial.println("Close");
     closed = true;
+<<<<<<< HEAD
+    if (order[action] == 1){
+      if(action == streak){
+        streak ++;
+        Serial.println("Performed :");
+        Serial.println(1);
+        played = true;
+      }
+      else{
+        action ++;
+        Serial.println("Performed :");
+        Serial.println(1);
+      }
+    }
+    else{
+      lost = true;
+      Serial.println("lost interrupteur");
+    }
+=======
     if (order[streak] == 1){
       streak ++;
     }
@@ -164,6 +235,7 @@ void loop(){
       lost = true;
     }
     played = true;
+>>>>>>> 682a301cec1ae3c69ab81c8ecdb5885f4772a042
   }
   else if (state != 0){
     //Serial.println("Open");
@@ -179,6 +251,28 @@ void loop(){
     {
        pressed = false;
     }
+<<<<<<< HEAD
+    else if (!pressed && buttonState == LOW)
+    {
+        if (order[action] == 2){
+      if(action == streak){
+        streak ++;
+        Serial.println("Performed :");
+        Serial.println(2);
+        played = true;
+      }
+      else{
+        action ++;
+        Serial.println("Performed :");
+        Serial.println(2);
+      }
+    }
+        else{
+          lost = true;
+          Serial.println("lost bouton");
+        }
+        pressed = true;
+=======
     else if (!pressed)
     {
         if (order[streak] == 2){
@@ -189,5 +283,6 @@ void loop(){
         }
         pressed = true;
         played = true;
+>>>>>>> 682a301cec1ae3c69ab81c8ecdb5885f4772a042
     }
 }
